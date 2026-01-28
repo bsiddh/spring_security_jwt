e6d833975c60cda5508da6817737171f9ced6c69
bsiddh <bsiddh667@gmail.com>
2026-01-29 00:15:19 +0530

    spring security initiated

---
+++ src/main/java/com/springsec/Springsecu/HelloController.java
@@ -0,0 +1,15 @@
+package com.springsec.Springsecu;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to telusko" + request.getSession().getId();
+
+    }
+}
